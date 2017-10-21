135f26f7f60dc597e9e2c4c6cbc718491327af86
lncogmito <lncogmitogames@gmail.com>
2017-10-21 15:58:24 +0200

    initialization

---
+++ src/main/java/com/spring/henallux/transAirPort/TransAirPortApplication.java
@@ -0,0 +1,12 @@
+package com.spring.henallux.transAirPort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransAirPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransAirPortApplication.class, args);
+	}
+}
