3b77f1430ecf18d0564577a3ac7f4547623a4997
lncogmito <lncogmitogames@gmail.com>
2017-11-23 11:59:14 +0100

    web security fonctionnel

--- src/main/java/com/spring/henallux/transAirPort/controller/AdminController.java
+++ src/main/java/com/spring/henallux/transAirPort/controller/AdminController.java
@@ -1,9 +1,18 @@
 package com.spring.henallux.transAirPort.controller;
 
+import com.spring.henallux.transAirPort.dataAccess.entity.UserEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value="/admin")
 public class AdminController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Model model){
+        model.addAttribute("user", new UserEntity());
+        return "integrated:admin";
+    }
 }
--- src/main/java/com/spring/henallux/transAirPort/dataAccess/util/ProviderConverter.java
+++ src/main/java/com/spring/henallux/transAirPort/dataAccess/util/ProviderConverter.java
@@ -1,5 +1,21 @@
 package com.spring.henallux.transAirPort.dataAccess.util;
 
+import com.spring.henallux.transAirPort.dataAccess.entity.UserEntity;
+import com.spring.henallux.transAirPort.model.User;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProviderConverter {
-    //SLIDE 24 ENTITY BEAN
+    private Mapper mapper = new DozerBeanMapper();
+
+    public UserEntity userModeltoUserEntity(User user){
+        return mapper.map(user,UserEntity.class);
+    }
+
+    public User userEntitytoUserModel(UserEntity userEntity){
+        return mapper.map(userEntity, User.class);
+    }
+
 }
